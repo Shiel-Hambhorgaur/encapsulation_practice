87a3457e92234e46c5abb3effa2a93677cab29a5
Shiel <rhezielabad3@gmail.com>
2025-09-13 17:05:03 +0800

    feat (PricklyFruit-3): add PricklyFruit-3 with unique property and method

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 import 'plant.dart';
+import 'pricklyfruit.dart';
 
 void main() {
   print('--- Demo: Base Plant ---');
 
   Plant plant1 = Plant(name: 'Rose', type: 'Flower Type Crop');
-  Plant plant2 = Plant(
-    name: 'Cactus',
-    type: 'Prickly Fruit',
-    growthStage: 3,
-    health: 80,
-  );
+  Plant plant2 = Plant(name: 'Cactus',type: 'Prickly Fruit',growthStage: 3,health: 80,);
 
   plant1.showInfo();
   plant1.grow();
@@ -21,4 +17,11 @@ void main() {
   plant2.showInfo();
   plant2.grow();
   plant2.water(-10);
+
+
+  print('--- Demo: PricklyFruit (derived) ---');
+  PricklyFruit dragonFruit = PricklyFruit(name: 'Dragon Fruit', type: 'Prickly Fruit', spikeLength: 7.5);
+  dragonFruit.showInfo(); // gi override
+  dragonFruit.grow(); // gi ovveride
+  dragonFruit.displaySpikes(); // unique method 
 }
